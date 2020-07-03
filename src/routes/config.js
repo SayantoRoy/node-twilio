55f38e06b907ee396667c8fa5ca3dfae3d6c760e
Sayanto Roy <sroy.sayanto@gmail.com>
2020-07-04 00:09:59 +0530

    Commit

--- src/routes/config.js
+++ src/routes/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serviceID:"VAf22a717ea678feba6b6a308abdafdc36",
-    accountSID:"ACbc512bd112f18798c895feec1bc16e75",
-    authToken:"de71f32220a4bf2f723dabb2a9a45927"
+    serviceID:"Service_id",
+    accountSID:"Acc_SID",
+    authToken:"Token"
 }
